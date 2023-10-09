0a112deb60a71b9ee1c03e0939c337ae7c08a737
Jiří Zapletal <jz@strategio.dev>
2023-10-08 19:05:52 -0600

    Fixed exports

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'megio-api',
-            fileName: 'megio-api',
+            name: 'megio-frontils',
+            fileName: 'megio-frontils',
         },
     },
     plugins: [dts()],
